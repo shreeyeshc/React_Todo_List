7a487d042d8efb65925e957d57404f69be088bf3
shreeyeshc <44208368+shreeyeshc@users.noreply.github.com>
2020-06-16 22:42:04 -0400

    Add files via upload

---
+++ build/precache-manifest.cf1f42b79849613e7f98b7fc00f76829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542dbd029908da17f7ed2fc1cd8ba2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4860b9f81cd58061d04d",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "6a2248ce92c24f3ec7a2",
+    "url": "/static/css/main.e3a8fd2e.chunk.css"
+  },
+  {
+    "revision": "4860b9f81cd58061d04d",
+    "url": "/static/js/2.a2c4f333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2c4f333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2248ce92c24f3ec7a2",
+    "url": "/static/js/main.dfe3ce52.chunk.js"
+  },
+  {
+    "revision": "ba126d3705b1cf6731bb",
+    "url": "/static/js/runtime-main.7fe061d9.js"
+  }
+]);
